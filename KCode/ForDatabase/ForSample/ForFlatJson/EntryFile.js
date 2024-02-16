a5fe2659f347d10cd47516923555f290362f7238
Keshav <nknnkumar@yahoo.com>
2024-02-16 20:53:55 +0530

    keshav

--- KCode/ForDatabase/ForSample/ForFlatJson/EntryFile.js
+++
@@ -1,9 +0,0 @@
-import { StartFunc as StartFuncFirstLevelEntryFile } from "./FirstLevelEntryFile.js";
-import { StartFunc as StartFuncSecondLevelEntryFile } from "./SecondLevelEntryFile.js";
-
-let StartFunc = () => {
-    StartFuncFirstLevelEntryFile();
-    StartFuncSecondLevelEntryFile();
-};
-
-export { StartFunc };
--- KCode/ForDatabase/ForSample/ForFlatJson/FirstLevelEntryFile.js
+++
@@ -1,8 +0,0 @@
-import ConfigJson from '../../../Config.json' assert {type: 'json'};
-import fs from "fs";
-
-let StartFunc = () => {
-    fs.mkdirSync(`${ConfigJson.ToDataDetails.DataPath}/${ConfigJson.ToDataDetails.DataPk}/kSampleFolder`);
-};
-
-export { StartFunc };
