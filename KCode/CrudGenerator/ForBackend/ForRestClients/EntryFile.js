655ab7d41bb9235b4b1c988118fd1fb036bae2ea
Keshav <nknnkumar@yahoo.com>
2024-02-18 23:26:10 +0530

    keshav

--- KCode/CrudGenerator/ForBackend/ForRestClients/EntryFile.js
+++ KCode/CrudGenerator/ForBackend/ForRestClients/EntryFile.js
@@ -53,6 +53,86 @@ let LocalFuncForTestEndPoint = ({ inTablesCollection, inTo, inFrom }) => {
 
     let LocalFirstLevelFolders = LocalTablesCollection.children.filter(element => {
         return "children" in element === false;
+    }).filter(element => element.name.endsWith(".json"))
+
+    // let LocalFirstLevelFolders = LocalTablesCollection.children.filter(element => {
+    //     return "children" in element === false;
+    // }).map(item => item.name).filter(element => element.name.endsWith(".json"))
+
+    // .map(item => item.name);
+    // console.log("aaaaaaaa : ", LocalFirstLevelFolders);
+
+    LocalFirstLevelFolders.forEach(element => {
+        let LoopInsideFileName = path.parse(element.name).name;
+        let LocalFilePath = `${LocalTo}/${LoopInsideFileName}/${LocalTypeName}`;
+
+        let LoopInsideFiles = fs.readdirSync(LocalFilePath, { withFileTypes: true })
+            .filter(item => !item.name.endsWith(".json"))
+            .map(item => item.name);
+
+
+        LoopInsideFiles.forEach(LoopFile => {
+            let LocalFileData = fs.readFileSync(`${LocalFilePath}/${LoopFile}`);
+
+            let LocalFileDataReplaced = LocalFileData.toString().replaceAll("ksSample", LoopInsideFileName);
+            let LocalBinReplaced = LocalFileDataReplaced.replaceAll(LocalFrom, LocalTo);
+
+            fs.writeFileSync(`${LocalFilePath}/${LoopFile}`, LocalBinReplaced);
+        });
+
+
+
+    });
+
+    // let k1 = fs.readdirSync('./KData/JSON/316', { withFileTypes: true })
+    //     .filter(item => !item.isDirectory())
+    //     .map(item => item.name);
+
+
+    // let LoopInsideFileName = path.parse(LoopFile.name).name;
+    // let LocalFilePath = `${LocalTo}/${LoopInsideFileName}/${LocalTypeName}/${LocalFileName}`;
+
+    // let LocalFileData = fs.readFileSync(LocalFilePath);
+
+    // StartFuncCommonFuncs({
+    //     inTypeName: LocalTypeName,
+    //     inFilesCollection: LocalFirstLevelFolders,
+    //     inTo: LocalTo, inFrom: LocalFrom,
+    //     inFileName: "1GetSchema.http"
+    // });
+
+    // StartFuncCommonFuncs({
+    //     inTypeName: LocalTypeName,
+    //     inFilesCollection: LocalFirstLevelFolders,
+    //     inTo: LocalTo, inFrom: LocalFrom,
+    //     inFileName: "2ReturnRows.http"
+    // });
+
+    // StartFuncCommonFuncs({
+    //     inTypeName: LocalTypeName,
+    //     inFilesCollection: LocalFirstLevelFolders,
+    //     inTo: LocalTo, inFrom: LocalFrom,
+    //     inFileName: "3InsertNewRow.http"
+    // });
+
+    // StartFuncCommonFuncs({
+    //     inTypeName: LocalTypeName,
+    //     inFilesCollection: LocalFirstLevelFolders,
+    //     inTo: LocalTo, inFrom: LocalFrom,
+    //     inFileName: "4InsertMultipleRows.http"
+    // });
+
+
+};
+let LocalFuncForTestEndPoint_18Feb2024 = ({ inTablesCollection, inTo, inFrom }) => {
+    let LocalTypeName = "restClients/testEndPoint";
+    let LocalTo = inTo;
+    let LocalFrom = inFrom;
+
+    let LocalTablesCollection = inTablesCollection;
+
+    let LocalFirstLevelFolders = LocalTablesCollection.children.filter(element => {
+        return "children" in element === false;
     });
 
     StartFuncCommonFuncs({
