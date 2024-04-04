568cb0096e8d557f2ec66863c5edccbbb8323808
veerendra17788 <vk7032332758@gmail.com>
2024-04-05 00:28:23 +0530

    veerendra first commit

---
+++ public/Accounts/Transactions/Openings/Delete/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -0,0 +1,45 @@
+let StartFunc = () => {
+    jFLocalHideSpinner();
+
+    var $table = $('#table');
+    // $table.bootstrapTable({
+    //     onClickRow: function (row, $element, field) {
+    //         console.log("aaaaaaaa : ", row, field, $element);
+    //     }
+    // });
+    $table.bootstrapTable({
+        onClickRow: jFLocalClickRow
+    });
+
+};
+
+let jFLocalHideSpinner = () => {
+    let jVarLocalSpinnerId = document.getElementById("SpinnerId");
+    jVarLocalSpinnerId.style.display = "none";
+};
+
+let jFLocalClickRow = (row, $element, field) => {
+    // console.log(field);
+    if (field === 3) {
+        jFLocalClickRowForDelete(row).then();
+    };
+};
+
+let jFLocalClickRowForDelete = async (inRow) => {
+    let LocalRowPk = inRow.pk;
+
+    let jVarLocalFetchUrl = `/bin/Accounts/${LocalRowPk}`;
+    let LocalBodyData = {
+        method: "DELETE"
+    };
+
+    let response = await fetch(jVarLocalFetchUrl, LocalBodyData);
+
+    if (response.status === 200) {
+        var $table = $('#table');
+  
+        $table.bootstrapTable('refresh');
+    };
+};
+
+export { StartFunc }
