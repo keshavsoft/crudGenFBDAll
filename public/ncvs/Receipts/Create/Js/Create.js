0c8fd668397bcebff3159974d9408bdee956cb23
veerendra17788 <vk7032332758@gmail.com>
2024-04-04 08:22:12 +0530

    vkfirst

--- public/ncvs/Receipts/Create/Js/Create.js
+++ public/ncvs/Receipts/Create/Js/Create.js
@@ -1,6 +1,8 @@
 import { StartFunc as StartFuncButtonClickFunc } from "./Addlisteners/SaveButtonClickId/ButtonClickFunc.js";
+import { StartFunc as StartFuncFormLoad } from "./FormLoad/EntryFile.js";
 
 const StartFunc = () => {
+    StartFuncFormLoad();
     StartFuncButtonClickFunc();
 };
 
