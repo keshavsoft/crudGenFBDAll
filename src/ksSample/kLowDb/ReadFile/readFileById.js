9abd02a68bc927bf08a763304a7e2a5e03ea933b
keshavsoft <nknnkumar@yahoo.com>
2024-04-05 11:00:11 +0530

    keshav

---
+++ src/ksSample/kLowDb/ReadFile/readFileById.js
@@ -0,0 +1,41 @@
+import { StartFunc as StartFuncPullData } from "../PullData/EntryFile.js";
+
+let StartFunc = async ({ inId }) => {
+  let LocalId = parseInt(inId);
+  
+  let LocalStartFuncPullData = StartFuncPullData();
+
+  if (LocalStartFuncPullData === false) {
+      LocalReturnData.KReason = LocalStartFuncPullData.KReason;
+      return LocalReturnData;
+  };
+
+  const LocalTableSchema = LocalStartFuncPullData.inTableSchema;
+  const db = LocalStartFuncPullData.inDb;
+  let LocalarrayOfObjects = db.data;
+
+  const LocalFindId = LocalarrayOfObjects.find((obj) => obj.pk === LocalId);
+
+  if (LocalFindId === undefined) {
+    return await { KTF: false, KReason: "Id not found in data" };
+  };
+
+  db.data = LocalFindId;
+  db.write();
+
+  return await true;
+};
+
+let deleteObjectById = ({ inCollection, inId }) => {
+  let LocalCollection = inCollection;
+  let LocalId = inId;
+
+  LocalCollection.splice(
+    LocalCollection.findIndex((a) => a.UuId === LocalId),
+    1
+  );
+
+  return LocalCollection;
+};
+
+export { StartFunc };
