e0cb0d7f18e33182ba372b4b4b6853c5abac8940
veerendra17788 <vk7032332758@gmail.com>
2024-04-06 17:26:35 +0530

    veerendra

--- public/Accounts/Masters/Accounts/Delete/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ public/Accounts/Masters/Accounts/Delete/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -21,7 +21,26 @@ let jFLocalHideSpinner = () => {
 let jFLocalClickRow = (row, $element, field) => {
     // console.log(field);
     if (field === 3) {
-        jFLocalClickRowForDelete(row).then();
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            
+          }).then((result) => {
+            if (result.isConfirmed) {
+                jFLocalClickRowForDelete(row).then();
+            //   Swal.fire({
+            //     title: "Deleted!",
+            //     text: "Your file has been deleted.",
+            //     icon: "success"
+            //   });
+            }
+          });
+        // jFLocalClickRowForDelete(row).then();
     };
 };
 
