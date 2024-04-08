da54fba7565696af0289e39710ff6e8eface5306
veerendra17788 <vk7032332758@gmail.com>
2024-04-08 14:55:14 +0530

    veerendra mult changes

--- public/Accounts/Transactions/Reciepts/Create/Js/FormLoad/ForDataList/AccountNames/GetFetch.js
+++ public/Accounts/Transactions/Reciepts/Create/Js/FormLoad/ForDataList/AccountNames/GetFetch.js
@@ -4,7 +4,8 @@ let StartFunc = async () => {
     let data = await response.json();
 
     localStorage.setItem("AccountNames", JSON.stringify(data));
-    jFLocalToDataList();
+    // jFLocalToDataList();
+    jFLocalToSelectOptions();
 };
 
 let jFLocalToDataList = () => {
@@ -21,4 +22,19 @@ let jFLocalToDataList = () => {
 
 };
 
+let jFLocalToSelectOptions = () => {
+    let jVarLocalArray = localStorage.getItem("AccountNames");
+    let jVarLocalArrayJson = JSON.parse(jVarLocalArray);
+    let str = "";
+
+    for (var i = 0; i < jVarLocalArrayJson.length; ++i) {
+        str +=  `<option value="${jVarLocalArrayJson[i].AccountName}">${jVarLocalArrayJson[i].AccountName}</option>`;
+
+        // str += '<option value="' + jVarLocalArrayJson[i].AccountName + '" />'; // Storing options in variable
+    };
+
+    let jVarLocalAccountNamesDataListId = document.getElementById("AccountNamesSelectId");
+    jVarLocalAccountNamesDataListId.innerHTML = str;
+
+};
 export { StartFunc };
