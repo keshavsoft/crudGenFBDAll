d0126f746c168d1d766c1def44c8da0fe2ca4cec
keshavsoft <nknnkumar@yahoo.com>
2024-04-11 16:12:48 +0530

    keshav

--- public/Accounts/Masters/Accounts/FooterInsert/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ public/Accounts/Masters/Accounts/FooterInsert/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -1,65 +1,30 @@
 import { StartFunc as StartFuncOnLoadSuccess } from "./OnLoadSuccess/EntryFile.js";
+import { StartFunc as StartFuncFetchFromEndPoint } from "./FetchFromEndPoint/EntryFile.js";
+import ConfigJson from '../../../Config.json' with {type: 'json'};
 
 let StartFunc = () => {
+    StartFuncFetchFromEndPoint();
     jFLocalHideSpinner();
-    
+    let jVarLocalDataNeeded = localStorage.getItem(ConfigJson.tableName);
+
     var $table = $('#table');
+    
+    // $table.bootstrapTable({
+    //     data: JSON.parse(jVarLocalDataNeeded),
+    //     onLoadSuccess: StartFuncOnLoadSuccess
+    // });
 
     $table.bootstrapTable({
-        onLoadSuccess: StartFuncOnLoadSuccess
+        data: JSON.parse(jVarLocalDataNeeded)
     });
+    
+    StartFuncOnLoadSuccess();
+
 };
 
 let jFLocalHideSpinner = () => {
     let jVarLocalSpinnerId = document.getElementById("SpinnerId");
     jVarLocalSpinnerId.style.display = "none";
-};
-
-let jFLocalSearchItemButtonId = () => {
-    let jVarLocalHtmlId = "searchButtonId";
-    let jVarLocaljVarLocalHtmlId = document.getElementById(jVarLocalHtmlId);
-    console.log("jVarLocaljVarLocalHtmlId : ", jVarLocaljVarLocalHtmlId);
-    jVarLocaljVarLocalHtmlId.addEventListener("click", jFLocalButtonClick);
-};
-
-let jFLocalButtonClick = () => {
-    const myModal = new bootstrap.Modal('#largeModal', {
-        keyboard: true
-    });
-
-    myModal.show();
-};
-
-let jFLocalButtonClick1 = () => {
-    let jVarLocalHtmlId = "largeModal";
-    let jVarLocallargeModal = document.getElementById(jVarLocalHtmlId);
-    console.log("jVarLocallargeModal : ", jVarLocallargeModal);
-    jVarLocallargeModal.addEventListener('shown.bs.modal', event => {
-        let jVarLocalHtmlId = "customSearch";
-        let jVarLocalcustomSearch = document.getElementById(jVarLocalHtmlId);
-        jVarLocalcustomSearch.focus();
-
-        var $table = $('#ModalItemsTable');
-
-        $table.bootstrapTable({
-            data: jFFromLocalStorage(),
-            onClickRow: jFLocalRowClick
-        });
-        // do something...
-    });
-
-    const myModal = new bootstrap.Modal('#largeModal', {
-        keyboard: false
-    });
-
-    myModal.show();
-
-
-    // $table.bootstrapTable({
-    //     onPostBody: function () {
-    //         $(".fixed-table-toolbar .search .search-input").focus()
-    //     }
-    // });
 };
 
 export { StartFunc }
--- public/Accounts/Masters/Accounts/FooterInsert/Js/FormLoad/AddListeners/RefreshBSTableId/OnLoadSuccess/EntryFile.js
+++ public/Accounts/Masters/Accounts/FooterInsert/Js/FormLoad/AddListeners/RefreshBSTableId/OnLoadSuccess/EntryFile.js
@@ -1,7 +1,10 @@
 import { StartFunc as StartFuncTableFooterAccountNameId } from "./TableFooterAccountNameId/EntryFile.js";
+import { StartFunc as StartFuncTableFooterAccountNameSearchButtonId } from "./TableFooterAccountNameSearchButtonId/EntryFile.js";
 
 let StartFunc = () => {
+    console.log("aaaaaaaa");
     StartFuncTableFooterAccountNameId();
+    StartFuncTableFooterAccountNameSearchButtonId();
 };
 
 export { StartFunc };
