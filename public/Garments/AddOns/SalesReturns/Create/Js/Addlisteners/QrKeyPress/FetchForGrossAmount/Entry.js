9f4f32e3bed1eddbf68fa841af2cb708a7640825
Keshav <nknnkumar@yahoo.com>
2024-04-17 11:41:01 +0530

    keshav

--- public/Garments/AddOns/SalesReturns/Create/Js/Addlisteners/QrKeyPress/FetchForGrossAmount/Entry.js
+++ public/Garments/AddOns/SalesReturns/Create/Js/Addlisteners/QrKeyPress/FetchForGrossAmount/Entry.js
@@ -4,19 +4,12 @@ import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
 let StartFunc = async ({ inRowpk }) => {
     let jVarLocalDataNeeded = await StartFuncFetchFuncs({ inRowpk });
-    console.log("jVarLocalDataNeeded : ", jVarLocalDataNeeded);
 
     if (jVarLocalDataNeeded.status === 200) {
         let jVarLocalFetchData = await jVarLocalDataNeeded.json();
 
         StartFuncAfterFetch({ inData: jVarLocalFetchData.JsonData });
     };
-
-    // if (jVarLocalDataNeeded !== null) {
-    //     if (jVarLocalDataNeeded) {
-    //        
-    //     };
-    // };
 };
 
 export { StartFunc }
--- public/Garments/AddOns/SalesReturns/Create/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/PostFetch.js
+++ public/Garments/AddOns/SalesReturns/Create/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/PostFetch.js
@@ -6,7 +6,7 @@ let StartFunc = async () => {
     let LocaltableName = ConfigJson.tableName;
 
     let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = `/${LocalroutePath}/${LocaltableName}`;
+    let jVarLocalFetchUrl = `/${LocalroutePath}/${LocaltableName}/GenUuId`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
     let data = await response.json();
 
