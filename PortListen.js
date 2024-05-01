4859576257c709139547ca3df06251e43348021c
AJAY KUMAR NANDIVADA <115331499+AJAYKUMAR-01@users.noreply.github.com>
2024-05-01 01:32:22 +0000

    Ajay [used predefined uuidv4]

--- PortListen.js
+++ PortListen.js
@@ -1,3 +1,5 @@
+const {v4: uuidv4} = require('uuid');
+
 let StartFunc = () => {
     console.log(`Listening in some local port : 7017`);
     console.log(`http server at http://localhost:7017`);
@@ -11,11 +13,11 @@ let LocalFuncCheckEnv = () => {
 
 };
 
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-};
+// function uuidv4() {
+//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+//         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+//         return v.toString(16);
+//     });
+// };
 
 export { StartFunc };
