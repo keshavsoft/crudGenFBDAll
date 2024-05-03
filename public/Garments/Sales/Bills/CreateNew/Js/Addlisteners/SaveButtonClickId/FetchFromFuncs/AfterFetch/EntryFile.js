01cd94698cf86816b61e8911022fb3fc232c4343
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-05-03 11:20:12 +0530

    ChangesDone

--- public/Garments/Sales/Bills/CreateNew/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ public/Garments/Sales/Bills/CreateNew/Js/Addlisteners/SaveButtonClickId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -3,14 +3,14 @@ let StartFunc = ({ inFetchData, inBodyData }) => {
 
     const url = new URL(window.location.href);
     const params1 = new URLSearchParams(url.search);
-    let NewURl = new URL("../Show/Show.html",url);
+
+    let NewURl = new URL("../LastBill/LastBill.html",url);
     const new_url = new URL(`${NewURl.href}?${params1}`);
+
     new_url.searchParams.append("RowPK", LocalinFetchData)
     new_url.searchParams.append("ToProducts", true)
     window.location.href = new_url.href;
     return;
-
-
 };
 
 export { StartFunc }
