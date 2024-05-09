f7ecd70c5e2ac2d58d6483f480e68fab30e408f7
JAMPANA MAHEETH SAI DURGA VARMA <21131a0581@gvpce.ac.in>
2024-05-09 12:59:41 +0530

    Remaining delete cases fixed with mongoDB

--- public/Accounts/Masters/Accounts/Delete/Delete-BSToast/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ public/Accounts/Masters/Accounts/Delete/Delete-BSToast/Js/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -1,18 +1,21 @@
-import { StartFunc as StartFuncBSTableLoad } from "./BSTableLoad.js";
+// import { StartFunc as StartFuncBSTableLoad } from "./BSTableLoad.js";
+import { StartFunc as StartFuncFetchFuncs } from "./Fetch.js";
 
-let StartFunc = () => {
-    jFLocalHideSpinner();
+import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
-    var $table = $('#table');
+let StartFunc = async () => {
+    // jFLocalHideSpinner();
 
-    $table.bootstrapTable({
-        onClickRow: StartFuncBSTableLoad
-    });
+    let jVarLocalDataNeeded = await StartFuncFetchFuncs();
+
+    if (jVarLocalDataNeeded !== null) {
+        StartFuncAfterFetch({ inDataToShow: jVarLocalDataNeeded });
+    };
 };
 
-let jFLocalHideSpinner = () => {
-    let jVarLocalSpinnerId = document.getElementById("SpinnerId");
-    jVarLocalSpinnerId.style.display = "none";
-};
+// let jFLocalHideSpinner = () => {
+//     let jVarLocalSpinnerId = document.getElementById("SpinnerId");
+//     jVarLocalSpinnerId.style.display = "none";
+// };
 
 export { StartFunc }
