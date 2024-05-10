0f93a22517c6f2ae2243fab0f679673b61a70a85
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-05-10 17:43:06 +0530

    keshav

--- KCode/Database.js
+++ KCode/Database.js
@@ -2,7 +2,7 @@ import { StartFunc as StartFuncReadDataSchema } from "./ReadDataSchema.js";
 import { StartFunc as StartFuncCrudGenerator } from "./CrudGenerator/Database.js";
 
 let LocalFilesArray = StartFuncReadDataSchema();
-console.log("LocalFilesArray : ", LocalFilesArray);
+// console.log("LocalFilesArray : ", LocalFilesArray);
 
 StartFuncCrudGenerator({ inFilesArray: LocalFilesArray });
 // console.log("aaaaaaaaaa : ", LocalFilesArray);
